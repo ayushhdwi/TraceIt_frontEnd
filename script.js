a32f183c3e2c4830963315a6487309ec9f7b0557
ayushhdwi <ayushhdwi@gmail.com>
2021-05-01 12:40:48 +0530

    script.js update

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const hamberger = document.querySelector(".hamburger");
 const line = document.querySelectorAll(".line");
 
 const options = document.querySelector(".options");
-const links = document.querySelectorAll(".options li");
 
 
 hamberger.addEventListener('click',() => {
@@ -16,25 +15,19 @@ hamberger.addEventListener('click',() => {
 	});
 	navbar.classList.toggle("purple");
 	brand.classList.toggle("brandOpen");
-
-	anime({
-        targets: ".brand.brandOpen",
-        translateX: 250,
-    });
-
 });
 
- // Scrolling Effect
- $(window).on("scroll", function () {
-     if ($(window).scrollTop()) {
-         $(".navbar").addClass("black");
-		 $(".line").forEach((whiteline) => {
-             whiteline.addClass("whiteLine");
-         });
-		} else {
-			$(".navbar").removeClass("black");
-			$(".line").forEach((whiteline) => {
-				whiteline.removeClass("whiteLine");
-			});
-     }
- });
+// Scrolling Effect
+$(window).on("scroll", function () {
+	if ($(window).scrollTop()) {
+		$(".navbar").addClass("black");
+		$(".line").forEach((whiteline) => {
+			whiteline.addClass("whiteLine");
+		});
+	} else {
+		$(".navbar").removeClass("black");
+		$(".line").forEach((whiteline) => {
+			whiteline.removeClass("whiteLine");
+		});
+	}
+});
